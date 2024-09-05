be5901b00ab2049a2741c0d754afbf343b489255
Ahkar02468 <arkarshwebaw@gmail.com>
2024-09-05 17:35:07 +0700

    Remove unused `fetchCardData` call and its variables in `Page` component.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,7 +9,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
      
-     const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData()
      return (
           <main>
                <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
