7509d4e4e8caf0d0a637bfeb029866fedfb4fddf
Ahkar02468 <arkarshwebaw@gmail.com>
2024-09-05 16:13:25 +0700

    add route group folder called (overview) to make effect of loading.tsx only for main page

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import DashboardSkeleton from "@/app/ui/skeletons"
+export default function Loading(){
+    return(
+        <div>
+            <h1><DashboardSkeleton/></h1>
+        </div>
+    )
+}
